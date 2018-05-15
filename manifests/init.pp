25cff606335d647e815c72a9ba69cda0fba9ea7e
root <root@ag1.wrk.int>
2018-05-15 15:37:49 -0400

    Add module manifest and config file

---
+++ manifests/init.pp
@@ -0,0 +1,13 @@
+# Manage NTP
+class ntp {
+  ensure_packages(['ntp'])
+  file { '/etc/ntp.conf':
+    source  => 'puppet:///modules/ntp/ntp.conf',
+    notify  => Service['ntp'],
+    require => Package['ntp'],
+  }
+  service { 'ntp':
+    ensure => running,
+    enable => true,
+  }
+}
